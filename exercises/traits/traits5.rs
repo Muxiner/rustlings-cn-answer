1769d55fef28afcd04f1cae6bfb37c84f85c4a13
Muxiner <muxinerhuang@gmail.com>
2023-05-19 02:48:22 +0800

    traits answer

--- exercises/traits/traits5.rs
+++ exercises/traits/traits5.rs
@@ -4,7 +4,7 @@
 // 不要修改此外的任何行。
 // 执行 `rustlings hint traits5` 或在观察模式下使用 `hint` 子命令来获取提示。
 
-// I AM NOT DONE
+// ？？ // I AM NOT DONE
 
 pub trait SomeTrait {
     fn some_function(&self) -> bool {
@@ -27,7 +27,8 @@ impl SomeTrait for OtherStruct {}
 impl OtherTrait for OtherStruct {}
 
 // 你只能修改下一行
-fn some_func(item: ??) -> bool {
+// fn some_func<T: SomeTrait + OtherTrait>(item: T) -> bool {
+fn some_func(item: impl SomeTrait + OtherTrait) -> bool{
     item.some_function() && item.other_function()
 }
 
