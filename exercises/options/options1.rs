720ad5027a0630cf91e285666af84afecc31aa7d
Muxiner <muxinerhuang@gmail.com>
2023-05-19 02:47:21 +0800

    options answer

--- exercises/options/options1.rs
+++ exercises/options/options1.rs
@@ -1,7 +1,7 @@
 // options1.rs
 // 执行 `rustlings hint options1` 或在观察模式下使用 `hint` 子命令来获取提示。
 
-// I AM NOT DONE
+// // I AM NOT DONE
 
 // 这个函数返回冰箱中还有多少冰淇淋。
 // 10PM 点前，这里有5根冰淇淋。
@@ -10,8 +10,20 @@ fn maybe_icecream(time_of_day: u16) -> Option<u16> {
     // 这里我们使用二十四小时制，所以 10PM 的值是22并且 12AM 的值是0
     // Option 输出需要很好地处理 time_of_day > 23 的情况。
     // TODO: 完成函数体 - 记住要返回一个 Option！
-    ???
+    match time_of_day {
+        n if n < 22 => Some(5),
+        n if 24 > n && n >= 22 => Some(0),
+        _ => None,
+    }
 }
+
+fn get_number(num: Option<u16>) -> u16 {
+    match num {
+        Some(i) => i,
+        None => 0,
+    }
+}
+
 
 #[cfg(test)]
 mod tests {
@@ -19,6 +31,7 @@ mod tests {
 
     #[test]
     fn check_icecream() {
+        println!("{:?}", maybe_icecream(9));
         assert_eq!(maybe_icecream(9), Some(5));
         assert_eq!(maybe_icecream(10), Some(5));
         assert_eq!(maybe_icecream(23), Some(0));
@@ -30,6 +43,6 @@ mod tests {
     fn raw_value() {
         // TODO: 修复这个测试。你要怎么获取 Option 内含的值？
         let icecreams = maybe_icecream(12);
-        assert_eq!(icecreams, 5);
+        assert_eq!(get_number(icecreams), 5);
     }
 }
