7bd1ee208e958fe3b3bed5652ab3da8e64b3260c
Muxiner <muxinerhuang@gmail.com>
2023-05-20 03:06:35 +0800

    iterators3 answer maybe just be one of a kind

--- exercises/iterators/iterators3.rs
+++ exercises/iterators/iterators3.rs
@@ -5,15 +5,17 @@
 // 2. 通过完成 result_with_list 和 list_of_results 函数使其余的测试通过。
 // 执行 `rustlings hint iterators3` 或在观察模式下使用 `hint` 子命令来获取提示。
 
-// I AM NOT DONE
+// // I AM NOT DONE
+use std::result::Result;
+use std::error::Error;
 
-#[derive(Debug, PartialEq, Eq)]
+#[derive(Debug, PartialEq, Eq, Copy, Clone)]
 pub enum DivisionError {
     NotDivisible(NotDivisibleError),
     DivideByZero,
 }
 
-#[derive(Debug, PartialEq, Eq)]
+#[derive(Debug, PartialEq, Eq, Copy, Clone)]
 pub struct NotDivisibleError {
     dividend: i32,
     divisor: i32,
@@ -22,21 +24,48 @@ pub struct NotDivisibleError {
 // 如果 'a' 可被 'b' 整除，计算 'a' 除以 'b'。
 // 不然，返回适当的错误。
 pub fn divide(a: i32, b: i32) -> Result<i32, DivisionError> {
-    todo!();
+    // todo!();
+    match b == 0 {
+        true => Err(DivisionError::DivideByZero),
+        false => match a % b == 0 {
+            true => Ok(a / b),
+            false => Err(DivisionError::NotDivisible(NotDivisibleError {
+                dividend: a,
+                divisor: b,
+            })),
+        },
+    }
 }
 
 // 完成函数并返回正确类型的值让测试通过。
 // 期待的输出： Ok([1, 11, 1426, 3])
-fn result_with_list() -> () {
+fn result_with_list() -> Result<Vec<i32>, Box<dyn Error>> {
     let numbers = vec![27, 297, 38502, 81];
-    let division_results = numbers.into_iter().map(|n| divide(n, 27));
+    let division_results: Vec<_> = numbers.into_iter().map(|n| divide(n, 27)).collect::<_>();
+    let mut vec_return = vec![];
+    for result in division_results.iter() {
+        if let Some(num) = result.as_ref().ok() {
+            vec_return.push(*num);
+        };
+    }
+
+    Ok(vec_return)
 }
 
 // 完成函数并返回正确类型的值让测试通过。
 // 期待的输出： [Ok(1), Ok(11), Ok(1426), Ok(3)]
-fn list_of_results() -> () {
+fn list_of_results() -> Vec<Result<i32, DivisionError>> {
     let numbers = vec![27, 297, 38502, 81];
-    let division_results = numbers.into_iter().map(|n| divide(n, 27));
+    let division_results: Vec<_> = numbers.into_iter().map(|n| divide(n, 27)).collect::<_>();
+    let mut vec_return = Vec::new();
+    
+    // division_results.iter().for_each(|x| vec_return.push(*x)); // 方法一
+    // 方法二
+    for result in division_results.iter() {
+        vec_return.push(*result);
+    }
+
+    vec_return
 }
 
 #[cfg(test)]
